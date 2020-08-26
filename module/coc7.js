d6f42d2f0cb38a42798c2454637d24940a9324a7
HavelockV <akhela0@gmail.com>
2020-08-26 02:06:04 +0200

    books&spells

--- module/coc7.js
+++ module/coc7.js
@@ -15,6 +15,9 @@ import { CoC7CharacterSheet } from './actors/sheets/actor-sheet.js';
 import { preloadHandlebarsTemplates } from './templates.js';
 import { CoC7Chat } from './chat.js';
 import { CoC7Combat } from './combat.js';
+import { CoC7BookSheet } from './items/sheets/book.js';
+import { CoC7SpellSheet } from './items/sheets/spell.js';
+import { COC7 } from './config.js';
 
 
 Hooks.once('init', async function() {
@@ -59,7 +62,6 @@ Hooks.once('init', async function() {
 			'unknown': 'SETTINGS.CheckDifficultyUnknown'
 		}
 	});
-
 	
 	// Set the use of token instead of portraits.
 	game.settings.register('CoC7', 'useToken', {
@@ -88,8 +90,27 @@ Hooks.once('init', async function() {
 	Actors.registerSheet('CoC7', CoC7CharacterSheet, { types: ['character'], makeDefault: true });
 	Items.unregisterSheet('core', ItemSheet);
 	Items.registerSheet('CoC7', CoC7WeaponSheet, { types: ['weapon'], makeDefault: true});
+	Items.registerSheet('CoC7', CoC7BookSheet, { types: ['book'], makeDefault: true});
+	Items.registerSheet('CoC7', CoC7SpellSheet, { types: ['spell'], makeDefault: true});
 	Items.registerSheet('CoC7', CoCItemSheet, { makeDefault: true});
 	preloadHandlebarsTemplates();
+});
+
+Hooks.once('setup', function() {
+
+	// Localize CONFIG objects once up-front
+	const toLocalize = [ 'spellProperties', 'bookType'];
+
+	for ( let o of toLocalize ) {
+		const localized = Object.entries(COC7[o]).map(e => {
+			return [e[0], game.i18n.localize(e[1])];
+		});
+		COC7[o] = localized.reduce((obj, e) => {
+			obj[e[0]] = e[1];
+			return obj;
+		}, {});
+	}	
+
 });
 
 Hooks.on('renderChatLog', (app, html, data) => CoC7Chat.chatListeners(app, html, data));
