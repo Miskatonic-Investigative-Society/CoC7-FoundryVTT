fd7c412e60f51ea85189f75d09134b34b9201936
HavelockV <akhela0@gmail.com>
2020-09-15 17:49:13 +0200

    Automatic damage implementation

--- module/config.js
+++ module/config.js
@@ -47,6 +47,7 @@ COC7.skillProperties = {
 COC7.weaponProperties = {
 	melee: 'CoC7.Weapon.Property.Melee',
 	rngd: 'CoC7.WeaponRngd',
+	mnvr: 'CoC7.WeaponProperyManeuver',
 	thrown: 'CoC7.Weapon.Property.Thrown',
 	shotgun: 'CoC7.Weapon.Property.Shotgun',
 	dbrl: 'CoC7.WeaponDbrl',
