65643d9607d3d9e732800d98c1aae5be6d663cdb
HavelockV <akhela0@gmail.com>
2021-02-05 17:13:23 +0100

    Link creation, bug on chat-card size.

--- README.md
+++ README.md
@@ -22,6 +22,12 @@ Compendium 'Roll Requests' contains numerous links for all your needs.
 
 ## What is working
 
+verison 0.5.1 :
+
+* Bug correction:
+  * Corrected a bug preventing attribute and sanity to be dragged onto a jounal entry to create a link.
+  * Corrected a bug on sanity chat-card where the full actor was embeded in the card resulting in high chat-log size. ( This is a temporary fix until refactorisation.)
+
 version 0.5.0 :
 
 * General reminder:
--- module/apps/parser.js
+++ module/apps/parser.js
@@ -31,6 +31,25 @@ import { RollDialog } from './roll-dialog.js';
 
 export class CoC7Parser{
 
+	// static async onDropSomething( canvas, item){
+	// 	let grid_size = canvas.scene.data.grid;
+	// 	const number_marked = canvas.tokens.targetObjects({
+	// 		x: item.x-grid_size/2,
+	// 		y: item.y-grid_size/2,
+	// 		height: grid_size,
+	// 		width: grid_size
+	// 	});
+	// 	if (number_marked) {
+	// 		// Change item type to avoid that Foundry processes it
+	// 		item.type = 'Custom';
+	// 		if (item.hasOwnProperty('id')) {
+	// 			game.macros.get(item.id).execute();
+	// 		} else {
+	// 			eval(item.data.command);
+	// 		}
+	// 	}
+	// }
+
 	static async onEditorDrop( event, editor){
 		event.preventDefault();
 
