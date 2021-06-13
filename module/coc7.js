2cc889523350f8c0909c3e743c28a5bbcf9dc7f8
James <43982555+snap01@users.noreply.github.com>
2021-06-13 10:26:10 +0100

    Run project's eslintrs.json

--- module/coc7.js
+++ module/coc7.js
@@ -779,39 +779,37 @@ Hooks.on('targetToken', function (user, token, targeted) {
 });
 
 class CompendiumDirectoryCoC7 extends CompendiumDirectory {
-  activateListeners (html) {
-    super.activateListeners(html)
-    let translated = false
-    if (game.i18n.lang === 'en') {
-      translated = true
-    } else if (typeof game.babele !== 'undefined') {
-      for (const [k, v] of Object.entries(game.babele.modules)) {
-        if (v.lang === game.i18n.lang) {
-          translated = true
-        }
-      }
-    }
-    if (!translated) {
-      html.find('footer.directory-footer').append('<a class="compendium-translation" title="' + game.i18n.localize('CoC7.HowToTranslateTitle') + '">' + game.i18n.localize('CoC7.HowToTranslateTitle') + '</a>')
-      html.find('.compendium-translation').click((e) => {
-        const message = '<p>' + game.i18n.localize('CoC7.HowToTranslateWarning') + '</p>' +
-          '<p>' + game.i18n.localize('CoC7.HowToTranslateInstallBabele') + '</p>' +
-          '<p>' + game.i18n.localize('CoC7.HowToTranslateInstallTranslation') + '</p>' +
-          '<p>' + game.i18n.localize('CoC7.HowToTranslateEnableTranslation') + '</p>' +
-          '<p>' + game.i18n.localize('CoC7.HowToTranslateNoTranslation') + '</p>'
-        new Dialog(
-          {
-            title: game.i18n.localize('CoC7.HowToTranslateTitle'),
-            content: message,
-            buttons: {},
-            default: 'close',
-            close: (html) => {
-            }
-          },
-          {}
-        ).render(true)
-      })
-    }
-  }
+	activateListeners (html) {
+		super.activateListeners(html);
+		let translated = false;
+		if (game.i18n.lang === 'en') {
+			translated = true;
+		} else if (typeof game.babele !== 'undefined') {
+			for (const v of Object.values(game.babele.modules)) {
+				if (v.lang === game.i18n.lang) {
+					translated = true;
+				}
+			}
+		}
+		if (!translated) {
+			html.find('footer.directory-footer').append('<a class="compendium-translation" title="' + game.i18n.localize('CoC7.HowToTranslateTitle') + '">' + game.i18n.localize('CoC7.HowToTranslateTitle') + '</a>');
+			html.find('.compendium-translation').click(() => {
+				const message = '<p>' + game.i18n.localize('CoC7.HowToTranslateWarning') + '</p>' +
+					'<p>' + game.i18n.localize('CoC7.HowToTranslateInstallBabele') + '</p>' +
+					'<p>' + game.i18n.localize('CoC7.HowToTranslateInstallTranslation') + '</p>' +
+					'<p>' + game.i18n.localize('CoC7.HowToTranslateEnableTranslation') + '</p>' +
+					'<p>' + game.i18n.localize('CoC7.HowToTranslateNoTranslation') + '</p>';
+				new Dialog(
+					{
+						title: game.i18n.localize('CoC7.HowToTranslateTitle'),
+						content: message,
+						buttons: {},
+						default: 'close'
+					},
+					{}
+				).render(true);
+			});
+		}
+	}
 }
 CONFIG.ui.compendium = CompendiumDirectoryCoC7;
