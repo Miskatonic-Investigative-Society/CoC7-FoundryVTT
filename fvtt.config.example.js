8da4e839178951a70e1cf12c1efe873d82c9cdb0
Castanho <joaocorr@outlook.com>
2021-08-26 15:07:22 -0300

    Add hint for dealing with fvtt.config.example.js

--- fvtt.config.example.js
+++ fvtt.config.example.js
@@ -8,11 +8,14 @@
  * And then you can create a system build that will be sent directly
  * to your Foundry folder by `npm run build` or `npm run watch`
  * See CONTRIBUTING.md, under .github/ directory for more details
+ * After rename `fvtt.config.example.js` to `fvtt.config.js` consider run:
+ * `git update-index --assume-unchanged fvtt.config.example.js`
+ * This way Git does not assume the original file has been deleted from the repository
  */
 
 const developmentOptions = {
   systemFolderName: 'CoC7',
-  userDataPath: 'PATH_TO_FOUNDRY_DATA_FOLDER_HERE'
+  userDataPath: 'PATH_TO_FOUNDRY_DATA_FOLDER'
 }
 
 export default developmentOptions
