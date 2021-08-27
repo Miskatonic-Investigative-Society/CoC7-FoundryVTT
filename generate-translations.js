3fec8a8c12e53a3cd99c7ff40c7c7e3967adfd8c
James <43982555+snap01@users.noreply.github.com>
2021-08-27 14:24:43 +0100

    Update translations page

--- generate-translations.js
+++ generate-translations.js
@@ -17,7 +17,11 @@ glob('./lang/*.json', {}, async function (er, files) {
       }
     })
   )
+  const complete = Object.keys(unordered).filter(
+    lang => unordered[lang].length === 0
+  )
   const langs = Object.keys(unordered)
+    .filter(lang => unordered[lang].length > 0)
     .sort()
     .reduce((obj, key) => {
       obj[key] = unordered[key]
@@ -27,33 +31,56 @@ glob('./lang/*.json', {}, async function (er, files) {
   output = output + '# Translating.\n\n'
   output =
     output +
-    'Thank you for being interested in making Call of Cthulhu 7th Edition for Foundry VTT better! Below is a list of translations keys on existing files that still need translated, based on `en.json`. Feel free to create a new `*.json` file for a language that is not shown here!\n\n'
-  output = output + '|Key|'
-  Object.entries(langs).forEach(([key, value]) => {
-    output = output + key + '|'
-  })
-  output = output + '\n'
-  output = output + '|:---|'
-  Object.entries(langs).forEach(([key, value]) => {
-    output = output + ':---:|'
-  })
-  output = output + '\n'
-  missing
-    .filter((item, index) => {
-      return missing.indexOf(item) === index
+    'Thank you for being interested in making Call of Cthulhu 7th Edition for Foundry VTT better!'
+  if (missing.length > 0) {
+    output =
+      output +
+      ' Below is a list of translations keys on existing files that still need translated, based on `en.json`.'
+  }
+  output =
+    output +
+    ' Feel free to create a new `*.json` file for a language that is not shown here!\n\n'
+  if (complete.length > 1) {
+    output =
+      output +
+      'The following translations are currently up to date **' +
+      complete.join('**, **') +
+      '**\n\n'
+  } else if (complete.length > 0) {
+    output =
+      output +
+      'The **' +
+      complete.join() +
+      '** translation is currently up to date\n\n'
+  }
+  if (missing.length > 0) {
+    output = output + '|Key|'
+    Object.entries(langs).forEach(([key, value]) => {
+      output = output + key + '|'
     })
-    .sort()
-    .forEach(key => {
-      output = output + '|' + key + '|'
-      Object.entries(langs).forEach(([lang, value]) => {
-        output = output + (value.includes(key) ? 'X' : '') + '|'
+    output = output + '\n'
+    output = output + '|:---|'
+    Object.entries(langs).forEach(([key, value]) => {
+      output = output + ':---:|'
+    })
+    output = output + '\n'
+    output = output + '|**Remaining**:|'
+    Object.entries(langs).forEach(([key, value]) => {
+      output = output + '**' + value.length + '**|'
+    })
+    output = output + '\n'
+    missing
+      .filter((item, index) => {
+        return missing.indexOf(item) === index
       })
-      output = output + '\n'
-    })
-  output = output + '|**Remaining**:|'
-  Object.entries(langs).forEach(([key, value]) => {
-    output = output + value.length + '|'
-  })
-  output = output + '\n'
+      .sort()
+      .forEach(key => {
+        output = output + '|' + key + '|'
+        Object.entries(langs).forEach(([lang, value]) => {
+          output = output + (value.includes(key) ? 'X' : '') + '|'
+        })
+        output = output + '\n'
+      })
+  }
   write('./.github/TRANSLATIONS.md', output)
 })
