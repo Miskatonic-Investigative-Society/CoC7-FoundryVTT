5f5ac48dab1dd54da690cf01fc9248fe711a49f5
Castanho <joaocorr@outlook.com>
2021-08-28 10:56:08 -0300

    Add new keys for books and update to book data

--- module/items/book/data.js
+++ module/items/book/data.js
@@ -6,29 +6,38 @@ export class CoC7Book extends CoC7Item {
     if (typeof data.img === 'undefined') data.img = 'icons/svg/book.svg'
     super(data, context)
   }
-  static convertDifficulty(difficulty) {
-    switch(difficulty) {
-      case 'hard': return CoC7Check.difficultyLevel.hard
-      case 'extreme': return CoC7Check.difficultyLevel.extreme
-      case 'critical': return CoC7Check.difficultyLevel.critical
-      case 'unreadable': return CoC7Check.difficultyLevel.impossible
-      default: return CoC7Check.difficultyLevel.regular
+  static convertDifficulty (difficulty) {
+    switch (difficulty) {
+      case 'hard':
+        return CoC7Check.difficultyLevel.hard
+      case 'extreme':
+        return CoC7Check.difficultyLevel.extreme
+      case 'critical':
+        return CoC7Check.difficultyLevel.critical
+      case 'unreadable':
+        return CoC7Check.difficultyLevel.impossible
+      default:
+        return CoC7Check.difficultyLevel.regular
     }
   }
-  async _changeProgress(mode, value) {
+  async _changeProgress (mode, value) {
     if (!this.isOwned) {
-      return ui.notifications.error('This item must be owned by an Actor for the Full Study progress count to be done.')
+      return ui.notifications.error(
+        'This item must be owned by an Actor for the Full Study progress count to be done.'
+      )
     }
     if (!this.data.data.initialReading) {
-      return ui.notifications.error(`${this.actor.name} need to perform an Initial Reading on <i>${this.name}</i> to be able to advance through Full Study progress.`)
+      return ui.notifications.error(
+        `${this.actor.name} need to perform an Initial Reading on <i>${this.name}</i> to be able to advance through Full Study progress.`
+      )
     }
     let fullStudy = this.data.data.fullStudy
     let progress = this.data.data.study.progress
     let necessary = this.data.data.study.necessary
     if (isNaN(progress)) {
       return await this.update({
-        'data.study.progress': 0,
-      });
+        'data.study.progress': 0
+      })
     }
     if (value && progress > value) {
       await this.update({
@@ -39,20 +48,20 @@ export class CoC7Book extends CoC7Item {
     }
     if (mode === 'increase' && progress < necessary) {
       await this.update({
-        'data.study.progress': ++progress,
-      });
+        'data.study.progress': ++progress
+      })
       if (progress === necessary) {
         await this.update({ 'data.fullStudy': ++fullStudy })
         return await this.completeFullStudy()
       }
     } else if (mode === 'decrease' && progress > 0) {
       return await this.update({
-        'data.study.progress': --progress,
-      });
+        'data.study.progress': --progress
+      })
     }
   }
 
-  async completeFullStudy() {
+  async completeFullStudy () {
     console.log(this)
   }
 
@@ -85,15 +94,18 @@ export class CoC7Book extends CoC7Item {
   }
 
   async listen (check) {
-
     Hooks.on('renderChatMessage', (data, html) => {
       if (data.id === check.referenceMessageId) {
         const rollResult = html.find('.roll-result')
-        if (rollResult.data('isSuccess'))
-        console.log(rollResult)
+        if (rollResult.data('isSuccess')) console.log(rollResult)
         console.log(html)
         if (rollResult.data('pushedRoll')) {
-          console.log(html.parent().parent().children('.chat-message'))
+          console.log(
+            html
+              .parent()
+              .parent()
+              .children('.chat-message')
+          )
 
           // console.log(html.parent().children())
         }
@@ -103,14 +115,18 @@ export class CoC7Book extends CoC7Item {
   }
 
   async _attemptInitialReading () {
-    const difficulty = CoC7Book.convertDifficulty(this.data.data.difficultyLevel)
+    const difficulty = CoC7Book.convertDifficulty(
+      this.data.data.difficultyLevel
+    )
     const language = this.data.data.language
     if (!language || language === '') {
-      await this.update({ 'data.language':  'English' })
+      await this.update({ 'data.language': 'English' })
     }
     let skill = this.actor.getSkillsByName(language)
     if (skill.length === 0) {
-      return ui.notifications.error(`${this.actor.name} does not know the ${language} language.`)
+      return ui.notifications.error(
+        `${this.actor.name} does not know the ${language} language.`
+      )
     } else {
       const check = new CoC7Check()
       check.actor = this.actor
@@ -120,7 +136,7 @@ export class CoC7Book extends CoC7Item {
       check.roll()
       await check.toMessage()
       if (check.passed) {
-        console.log("!!!")
+        console.log('!!!')
       } else {
         await this.listen(check)
       }
@@ -133,32 +149,32 @@ export class CoC7Book extends CoC7Item {
     //   await this.actor.didInitialReading(book)
     // }
     // async attemptInitialReading(book) {
-      // let language = book.data.data.language;
-      // let difficulty = book.data.data.difficultyLevel;
-      // language == '' || language == null ? language = 'English' : false;
-      // difficulty = CoC7Utilities.convertDifficulty(difficulty);
-      // let skill = this.getSkillsByName(language);
-      // if (skill.length != 0) {
-      //   skill = skill[0];
-      //   let check = new CoC7Check();
-      //   CoC7Dice.showRollDice3d(check);
-      //   check.actor = this.tokenKey;
-      //   check.skill = skill;
-      //   check.difficulty = difficulty;
-      //   check.roll();
-      //   let attemptToChat = new BookProgress();
-      //   attemptToChat.actor = this.tokenKey;
-      //   attemptToChat.actorKey = this.actorKey;
-      //   attemptToChat.fullStudy = false;
-      //   attemptToChat.book = book;
-      //   attemptToChat.sanLoss = book.data.data.sanLoss;
-      //   attemptToChat.check = check;
-      //   //check.toMessage();
-      //   attemptToChat.toMessage();
-      //   return check.passed;
-      // } else {
-      //   ui.notifications.warn(game.i18n.format('CoC7.notKnowLanguage', {actor: this.name, language: language}));
-      // }
+    // let language = book.data.data.language;
+    // let difficulty = book.data.data.difficultyLevel;
+    // language == '' || language == null ? language = 'English' : false;
+    // difficulty = CoC7Utilities.convertDifficulty(difficulty);
+    // let skill = this.getSkillsByName(language);
+    // if (skill.length != 0) {
+    //   skill = skill[0];
+    //   let check = new CoC7Check();
+    //   CoC7Dice.showRollDice3d(check);
+    //   check.actor = this.tokenKey;
+    //   check.skill = skill;
+    //   check.difficulty = difficulty;
+    //   check.roll();
+    //   let attemptToChat = new BookProgress();
+    //   attemptToChat.actor = this.tokenKey;
+    //   attemptToChat.actorKey = this.actorKey;
+    //   attemptToChat.fullStudy = false;
+    //   attemptToChat.book = book;
+    //   attemptToChat.sanLoss = book.data.data.sanLoss;
+    //   attemptToChat.check = check;
+    //   //check.toMessage();
+    //   attemptToChat.toMessage();
+    //   return check.passed;
+    // } else {
+    //   ui.notifications.warn(game.i18n.format('CoC7.notKnowLanguage', {actor: this.name, language: language}));
+    // }
     // }
   }
   async _onSpellDelete (event) {
