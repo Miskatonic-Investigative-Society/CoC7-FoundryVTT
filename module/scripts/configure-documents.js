8dad2694136a3ef8566b8ee17b41294afb2787b5
Castanho <joaocorr@outlook.com>
2021-08-30 13:52:25 -0300

    Register book document class to configure documents

--- module/scripts/configure-documents.js
+++ module/scripts/configure-documents.js
@@ -1,11 +1,14 @@
 /* global CONFIG */
 
 import { CoCActor } from '../actors/actor.js'
+import { CoC7Book } from '../items/book/data.js'
 import { CoC7Item } from '../items/item.js'
 
 export function configureDocuments () {
   CONFIG.Actor.documentClass = CoCActor
   CONFIG.Actor.documentClasses = {}
   CONFIG.Item.documentClass = CoC7Item
-  CONFIG.Item.documentClasses = {}
+  CONFIG.Item.documentClasses = {
+    book: CoC7Book
+  }
 }
