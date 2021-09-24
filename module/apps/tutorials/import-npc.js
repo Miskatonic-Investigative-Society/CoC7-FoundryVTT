f1f9009ee1671036d2cd5c09256447588c0d81c9
snap01 <43982555+snap01@users.noreply.github.com>
2021-09-24 16:03:55 +0100

    Importer has to run in English until all translations have regExs

--- module/apps/tutorials/import-npc.js
+++ module/apps/tutorials/import-npc.js
@@ -22,6 +22,7 @@ export const importNPC = {
       prompt: 'CoC7.Tutorial.ImportNPC.2',
       setup: (options) => {
         const text = 'Example Character, age 27\nSTR 75 CON 60 SIZ 80 DEX 70 APP 60 INT 80\nPOW 50 EDU 85 SAN 55 HP 14 DB: 1D4\nBuild: 1 Move: 7 MP: 10 Luck: 40 Armor: 1\nSAN loss: 1d4/1d8 # Attacks: 3\nCombat\nBite 50% (25/10), damage 1D6\nBrawl 30% (15/6), damage 1D3\nDerringer 40% (20/8), damage 1D8+1\nDodge 50% (25/10)\nSkills\nAnimal Handling 55%, Charm 30%, First Aid 25%, Disguise 20%,\nListen 50%, Medicine 45%, Persuade 25%, Psychology 75%,\nScience (Astronomy) 90%, Science (Botany) 35%, Science (Zoology) 10%,\nSpot Hidden 35%, Stealth 10%\nLanguages: English 80%, Eklo 5%.\nSpells: Summon NPC, Dispel NPC.'
+        $('#coc-entity-lang').val('coc-en')
         $('#coc-pasted-character-data').click(function () {
           $(this).val(text).blur().off('click')
           game.CoC7Tutorial.nextStep()
