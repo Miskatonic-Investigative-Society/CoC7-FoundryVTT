29a2b87accc1a1530554bc4821ac82644514a2f0
castanho <castanhocorreia.dev@gmail.com>
2021-10-11 16:36:13 -0300

    Fix augment roll formula

--- module/actors/actor.js
+++ module/actors/actor.js
@@ -237,15 +237,15 @@ export class CoCActor extends Actor {
     }
     let boutDurationText = this.isInABoutOfMadness
       ? boutRealTime
-        ? `${duration} ${game.i18n.localize('CoC7.rounds')}`
-        : `${duration} ${game.i18n.localize('CoC7.hours')}`
+          ? `${duration} ${game.i18n.localize('CoC7.rounds')}`
+          : `${duration} ${game.i18n.localize('CoC7.hours')}`
       : null
     const insanityDurationText = insaneDuration
       ? this.isInsane
-        ? indefiniteInstanity
-          ? null
-          : `${insaneDuration} ${game.i18n.localize('CoC7.hours')}`
-        : null
+          ? indefiniteInstanity
+              ? null
+              : `${insaneDuration} ${game.i18n.localize('CoC7.hours')}`
+          : null
       : null
     if (this.isInsane && !insanityDurationText && !indefiniteInstanity) {
       indefiniteInstanity = true
@@ -272,8 +272,8 @@ export class CoCActor extends Actor {
         durationText: insanityDurationText || '',
         hint: this.isInsane
           ? indefiniteInstanity
-            ? game.i18n.localize('CoC7.IndefiniteInsanity')
-            : `${game.i18n.localize(
+              ? game.i18n.localize('CoC7.IndefiniteInsanity')
+              : `${game.i18n.localize(
                 'CoC7.TemporaryInsanity'
               )} ${insanityDurationText || ''}`
           : game.i18n.localize('CoC7.NotInsane')
@@ -2958,7 +2958,7 @@ export class CoCActor extends Actor {
     let augmentRoll
     if (pulpRulesActivated) {
       equalOrLessCurrentLuck
-        ? (augmentRoll = '2D10+5')
+        ? (augmentRoll = '2D10+10')
         : (augmentRoll = '1D10+5')
     } else if (equalOrLessCurrentLuck) {
       augmentRoll = '1D10'
