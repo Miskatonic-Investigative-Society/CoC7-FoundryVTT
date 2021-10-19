025b6f07bf1a081c0816a1a891bc1cbdf417987d
snap01 <43982555+snap01@users.noreply.github.com>
2021-10-19 12:49:55 +0100

    Send inventory book id to spell

--- module/items/book/data.js
+++ module/items/book/data.js
@@ -1,4 +1,4 @@
-/* global ChatMessage, duplicate, foundry, game, renderTemplate, Roll, ui */
+/* global ChatMessage, duplicate, game, renderTemplate, Roll, ui */
 
 import { SanCheckCard } from '../../chat/cards/san-check.js'
 import { CoC7Check } from '../../check.js'
@@ -35,14 +35,11 @@ export class CoC7Book extends CoC7Item {
    * @returns {Promise<Document>} update to Item document
    */
   async addSpells (spells) {
-    console.log(spells)
     const collection = this.data.data.spells
       ? duplicate(this.data.data.spells)
       : []
     for (const spell of spells) {
-      const { _source, _id, ...data } = spell
-      data._id = foundry.utils.randomID(16)
-      collection.push(data)
+      collection.push(spell)
     }
     return await this.update({ 'data.spells': collection })
   }
@@ -50,19 +47,8 @@ export class CoC7Book extends CoC7Item {
   async spellDetail (index) {
     const data = this.data.data.spells[index]
     const parent = this.actor ? this.actor : null
-    const spell = new CoC7Spell(data, { parent })
-    await spell.update({ 'data.flags.book': this.id, 'data.flags.index': index })
+    const spell = new CoC7Spell(data, { parent, bookId: this.id })
     return await spell.sheet.render(true)
-  }
-
-  async updateSpell (index, data) {
-    console.log(data)
-    const collection = this.data.data.spells
-      ? duplicate(this.data.data.spells)
-      : []
-    collection[index] = data
-    await this.update({ 'data.spells': collection })
-    return this.sheet.render(true)
   }
 
   /**
