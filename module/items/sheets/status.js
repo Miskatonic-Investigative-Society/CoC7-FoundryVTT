7df1b55389362f9c78bac9347247d4af6f248b11
snap01 <43982555+snap01@users.noreply.github.com>
2022-09-15 20:42:39 +0100

    Test merge

--- module/items/sheets/status.js
+++ module/items/sheets/status.js
@@ -1,4 +1,6 @@
-/* global game, ItemSheet, mergeObject, TextEditor */
+/* global game, ItemSheet, mergeObject */
+
+import CoC7ActiveEffect from '../../active-effect.js'
 import { COC7 } from '../../config.js'
 
 /**
@@ -11,8 +13,7 @@ export class CoC7StatusSheet extends ItemSheet {
   static get defaultOptions () {
     return mergeObject(super.defaultOptions, {
       classes: ['coc7', 'sheet', 'status'],
-      template: 'systems/CoC7/templates/items/status.html',
-      width: 525,
+      width: 520,
       height: 480,
       scrollY: ['.tab.description'],
       tabs: [
@@ -25,38 +26,46 @@ export class CoC7StatusSheet extends ItemSheet {
     })
   }
 
+  /**
+   *
+   */
+  get template () {
+    return 'systems/CoC7/templates/items/status.html'
+  }
+
+  activateListeners (html) {
+    super.activateListeners(html)
+
+    html
+      .find('.effect-control')
+      .click(ev => CoC7ActiveEffect.onManageActiveEffect(ev, this.item))
+  }
+
   /* Prepare data for rendering the Item sheet
    * The prepared data object contains both the actor data as well as additional sheet options
    */
   getData () {
-    const sheetData = super.getData()
+    // this.item.checkSkillProperties();
+    const data = super.getData()
 
-    sheetData.itemProperties = []
+    data.effects = CoC7ActiveEffect.prepareActiveEffectCategories(this.item.effects, { status: false })
 
-    for (const [key, value] of Object.entries(this.item.system.type)) {
+    /** MODIF: 0.8.x **/
+    const itemData = data.data
+    data.data = itemData.data // MODIF: 0.8.x data.data
+    /*****************/
+
+    data.itemProperties = []
+
+    for (const [key, value] of Object.entries(data.data.type)) {
       if (value) {
-        sheetData.itemProperties.push(
+        data.itemProperties.push(
           COC7.statusType[key] ? COC7.statusType[key] : null
         )
       }
     }
 
-    sheetData.enrichedDescriptionValue = TextEditor.enrichHTML(
-      sheetData.data.system.description.value,
-      { async: false }
-    )
-
-    sheetData.enrichedDescriptionNotes = TextEditor.enrichHTML(
-      sheetData.data.system.description.notes,
-      { async: false }
-    )
-
-    sheetData.enrichedDescriptionKeeper = TextEditor.enrichHTML(
-      sheetData.data.system.description.keeper,
-      { async: false }
-    )
-
-    sheetData.isKeeper = game.user.isGM
-    return sheetData
+    data.isKeeper = game.user.isGM
+    return data
   }
 }
