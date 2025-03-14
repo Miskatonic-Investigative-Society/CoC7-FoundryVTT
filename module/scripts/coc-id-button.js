613d03e6856122dffbcca32b4df0048878e39dbb
snap01 <43982555+snap01@users.noreply.github.com>
2025-03-14 10:52:53 +0000

    Incomplete FoundryVTT V13 support

--- module/scripts/coc-id-button.js
+++ module/scripts/coc-id-button.js
@@ -3,14 +3,20 @@ import { CoCIDEditor } from '../apps/coc-id-editor.js'
 
 export function addCoCIDSheetHeaderButton (headerButtons, sheet) {
   if (game.user.isGM) {
-    const sheetCoCID = sheet.object.flags?.CoC7?.cocidFlag
+    if (typeof sheet.options.actions !== 'undefined') {
+      sheet.options.actions.cocid = () => {
+        new CoCIDEditor(sheet.document, {}).render(true, { focus: true })
+      }
+    }
+    const sheetCoCID = sheet.document.flags?.CoC7?.cocidFlag
     const noId = (typeof sheetCoCID === 'undefined' || typeof sheetCoCID.id === 'undefined' || sheetCoCID.id === '')
     const CoCIDEditorButton = {
       class: (noId ? 'edit-coc-id-warning' : 'edit-coc-id-exisiting'),
       label: 'CoC7.CoCIDFlag.id',
       icon: 'fas fa-fingerprint',
+      action: 'cocid',
       onclick: () => {
-        new CoCIDEditor(sheet.object, {}).render(true, { focus: true })
+        new CoCIDEditor(sheet.document, {}).render(true, { focus: true })
       }
     }
     const numberOfButtons = headerButtons.length
