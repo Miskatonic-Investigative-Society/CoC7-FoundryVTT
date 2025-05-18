5f3f9e5143fa77f02999faa44c21b999da4e96e6
snap01 <43982555+snap01@users.noreply.github.com>
2025-05-18 11:41:06 +0100

    Fix Hotbar Drop Hook not returning flag

--- module/hooks/hotbar-drop.js
+++ module/hooks/hotbar-drop.js
@@ -1,5 +1,5 @@
 import { CoC7Utilities } from '../utilities.js'
 
 export default function (bar, data, slot) {
-  CoC7Utilities.createMacro(bar, data, slot)
+  return CoC7Utilities.createMacro(bar, data, slot)
 }
