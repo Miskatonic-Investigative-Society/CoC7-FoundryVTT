a38556a0a208580b44e98dc9e51459e457c4a372
ryotai-trpg <ryotai1025@yahoo.co.jp>
2025-09-21 20:44:48 +0900

    remove: RenderDialog

--- module/coc7.js
+++ module/coc7.js
@@ -36,7 +36,6 @@ import RenderCoC7CreatureSheet from './hooks/render-coc7-creature-sheet.js'
 import RenderCoC7DirectoryPicker from './hooks/render-coc7-directory-picker.js'
 import RenderCoC7NPCSheet from './hooks/render-coc7-npc-sheet.js'
 import RenderCombatTracker from './hooks/render-combat-tracker.js'
-import RenderDialog from './hooks/render-dialog.js'
 import RenderGamePause from './hooks/render-game-pause.js'
 import RenderItemSheet from './hooks/render-item-sheet.js'
 import RenderJournalEntryPageTextSheet from './hooks/render-journal-entry-page-text-sheet.js'
@@ -93,7 +92,6 @@ Hooks.on('renderCoC7CreatureSheet', RenderCoC7CreatureSheet)
 Hooks.on('renderCoC7DirectoryPicker', RenderCoC7DirectoryPicker)
 Hooks.on('renderCoC7NPCSheet', RenderCoC7NPCSheet)
 Hooks.on('renderCombatTracker', RenderCombatTracker)
-Hooks.on('renderDialog', RenderDialog)
 Hooks.on('renderGamePause', RenderGamePause)
 Hooks.on('renderItemSheet', RenderItemSheet)
 Hooks.on('renderJournalEntryPageTextSheet', RenderJournalEntryPageTextSheet)
